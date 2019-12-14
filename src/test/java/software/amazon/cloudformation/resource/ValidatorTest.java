4ae67bf6f39603870adf0e270b2a7657b55df781
Vladimir Tsirushkin <tsirushv@amazon.com>
2019-12-14 15:51:56 -0800

    Validate $ref correctly (#23)

--- src/test/java/software/amazon/cloudformation/resource/ValidatorTest.java
+++ src/test/java/software/amazon/cloudformation/resource/ValidatorTest.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.jupiter.api.BeforeEach;
@@ -35,6 +36,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import software.amazon.cloudformation.resource.exceptions.ValidationException;
 
 public class ValidatorTest {
+    private static final String RESOURCE_DEFINITION_SCHEMA_PATH = "/schema/provider.definition.schema.v1.json";
     private static final String TEST_SCHEMA_PATH = "/test-schema.json";
     private static final String TEST_VALUE_SCHEMA_PATH = "/scrubbed-values-schema.json";
     private static final String TYPE_NAME_KEY = "typeName";
@@ -333,14 +335,6 @@ public class ValidatorTest {
         validator.validateResourceDefinition(definition);
     }
 
-    @Test
-    public void validateDefinition_validRelativeRef_shouldNotThrow() {
-        final JSONObject definition = new JSONObject(new JSONTokener(this.getClass()
-            .getResourceAsStream("/valid-with-relative-ref.json")));
-
-        validator.validateResourceDefinition(definition);
-    }
-
     @ParameterizedTest
     @ValueSource(strings = { "ftp://example.com", "http://example.com", "git://example.com", "https://", })
     public void validateDefinition_nonMatchingDocumentationUrl_shouldThrow(final String documentationUrl) {
@@ -418,4 +412,42 @@ public class ValidatorTest {
         }
     }
 
+    /**
+     * trivial coverage test: cannot cache a schema if it has an invalid $id
+     */
+    @ParameterizedTest
+    @ValueSource(strings = { ":invalid/uri", "" })
+    public void registerMetaSchema_invalidRelativeRef_shouldThrow(String uri) {
+
+        JSONObject badSchema = loadJSON(RESOURCE_DEFINITION_SCHEMA_PATH);
+        badSchema.put("$id", uri);
+        assertThatExceptionOfType(ValidationException.class).isThrownBy(() -> {
+            validator.registerMetaSchema(SchemaLoader.builder(), badSchema);
+        });
+    }
+
+    /**
+     * trivial coverage test: cannot cache a schema if it has no $id
+     */
+    @Test
+    public void registerMetaSchema_nullId_shouldThrow() {
+        JSONObject badSchema = loadJSON(RESOURCE_DEFINITION_SCHEMA_PATH);
+        badSchema.remove("$id");
+        assertThatExceptionOfType(ValidationException.class).isThrownBy(() -> {
+            validator.registerMetaSchema(SchemaLoader.builder(), badSchema);
+        });
+    }
+
+    static JSONObject loadJSON(String path) {
+        try {
+            return new JSONObject(new JSONTokener(ValidatorTest.getResourceAsStream(path)));
+        } catch (Throwable ex) {
+            System.out.println("path: " + path);
+            throw ex;
+        }
+    }
+
+    static InputStream getResourceAsStream(String path) {
+        return ValidatorRefResolutionTests.class.getResourceAsStream(path);
+    }
 }
