1f044ad0b260ac3e06f282c9b32f6f5eb6ec851c
Narendra Maddhika <narmadn@amazon.com>
2021-07-06 16:22:55 -0700

    Check if properties list is empty before attempting to remove

--- src/main/java/software/amazon/cloudformation/resource/ResourceTypeSchema.java
+++ src/main/java/software/amazon/cloudformation/resource/ResourceTypeSchema.java
@@ -222,20 +222,22 @@ public class ResourceTypeSchema {
 
     public static void removeProperty(final PublicJSONPointer property, final JSONObject resourceModel) {
         List<String> refTokens = property.getRefTokens();
-        final String key = refTokens.get(refTokens.size() - 1);
-        try {
-            // if size is more than one, fetch parent object/array of key to remove so that
-            // we can remove
-            if (refTokens.size() > 1) {
-                // use sublist to specify to point at the parent object
-                final JSONPointer parentObjectPointer = new JSONPointer(refTokens.subList(0, refTokens.size() - 1));
-                final JSONObject parentObject = (JSONObject) parentObjectPointer.queryFrom(resourceModel);
-                parentObject.remove(key);
-            } else {
-                resourceModel.remove(key);
+        if (refTokens.size() > 0) {
+            final String key = refTokens.get(refTokens.size() - 1);
+            try {
+                // if size is more than one, fetch parent object/array of key to remove so that
+                // we can remove
+                if (refTokens.size() > 1) {
+                    // use sublist to specify to point at the parent object
+                    final JSONPointer parentObjectPointer = new JSONPointer(refTokens.subList(0, refTokens.size() - 1));
+                    final JSONObject parentObject = (JSONObject) parentObjectPointer.queryFrom(resourceModel);
+                    parentObject.remove(key);
+                } else {
+                    resourceModel.remove(key);
+                }
+            } catch (JSONPointerException | NumberFormatException e) {
+                // do nothing, as this indicates the model does not have a value for the pointer
             }
-        } catch (JSONPointerException | NumberFormatException e) {
-            // do nothing, as this indicates the model does not have a value for the pointer
         }
     }
 
