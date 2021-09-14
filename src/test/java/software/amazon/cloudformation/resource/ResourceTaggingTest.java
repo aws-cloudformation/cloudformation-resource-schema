135a87ed90edea0ffffd1fb7a76112fee201e57f
Yunhao Jiao <jyunhao@amazon.com>
2021-09-14 10:58:49 -0700

    Fix unit test failures for ResourceTagging class

--- src/test/java/software/amazon/cloudformation/resource/ResourceTaggingTest.java
+++ src/test/java/software/amazon/cloudformation/resource/ResourceTaggingTest.java
@@ -1,16 +1,30 @@
+/*
+* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 package software.amazon.cloudformation.resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.everit.json.schema.JSONPointer;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResourceTaggingTest {
     @Test
     public void testResetTaggable() {
 
         final ResourceTagging resourceTagging = new ResourceTagging(true, true, true,
-            true, new JSONPointer("/properties/tags"));
+                                                                    true, new JSONPointer("/properties/tags"));
         resourceTagging.resetTaggable(false);
 
         assertThat(resourceTagging.isTaggable()).isEqualTo(false);
